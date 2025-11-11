91e5ddef1da7544f2104b3b0d200dd17684effdd
JJ-best <jesse.mojo@gmail.com>
2025-11-11 20:47:09 +0800

    2025.11.11 mean

--- HW5/hdl/ViT_top.v
+++ HW5/hdl/ViT_top.v
@@ -242,64 +242,112 @@ end
 // since we need 2 cycle to get complete token, so we need to store the 
 // first half cycle data, and connect to next cycle data
 // ===== store data per channel ===== //
-reg [BW_PER_ACT-1:0] a_bank0_reg [0:CH_NUM-1];
-reg [BW_PER_ACT-1:0] a_bank1_reg [0:CH_NUM-1];
-reg [BW_PER_ACT-1:0] a_bank2_reg [0:CH_NUM-1];
-reg [BW_PER_ACT-1:0] a_bank3_reg [0:CH_NUM-1];
+reg [BW_PER_ACT-1:0] a_bank0_reg_h [0:CH_NUM-1];
+reg [BW_PER_ACT-1:0] a_bank1_reg_h [0:CH_NUM-1];
+reg [BW_PER_ACT-1:0] a_bank2_reg_h [0:CH_NUM-1];
+reg [BW_PER_ACT-1:0] a_bank3_reg_h [0:CH_NUM-1];
+reg [BW_PER_ACT-1:0] a_bank0_reg_l [0:CH_NUM-1];
+reg [BW_PER_ACT-1:0] a_bank1_reg_l [0:CH_NUM-1];
+reg [BW_PER_ACT-1:0] a_bank2_reg_l [0:CH_NUM-1];
+reg [BW_PER_ACT-1:0] a_bank3_reg_l [0:CH_NUM-1];
+
+reg valid_1;
 
 // ---- bank0 ---- //
 always @(posedge clk) begin
     if (sram_addr_a0[4] == 1'b1) begin
-        a_bank0_reg[0] <= a_bank0_dat[0];
-        a_bank0_reg[1] <= a_bank0_dat[1];
-        a_bank0_reg[2] <= a_bank0_dat[2];
-        a_bank0_reg[3] <= a_bank0_dat[3];
-        a_bank0_reg[4] <= a_bank0_dat[4];
-        a_bank0_reg[5] <= a_bank0_dat[5];
-        a_bank0_reg[6] <= a_bank0_dat[6];
-        a_bank0_reg[7] <= a_bank0_dat[7];
+        a_bank0_reg_h[0] <= a_bank0_dat[0];
+        a_bank0_reg_h[1] <= a_bank0_dat[1];
+        a_bank0_reg_h[2] <= a_bank0_dat[2];
+        a_bank0_reg_h[3] <= a_bank0_dat[3];
+        a_bank0_reg_h[4] <= a_bank0_dat[4];
+        a_bank0_reg_h[5] <= a_bank0_dat[5];
+        a_bank0_reg_h[6] <= a_bank0_dat[6];
+        a_bank0_reg_h[7] <= a_bank0_dat[7];
+        valid_1 <= 1;
+    end
+    if (sram_addr_a0[4] == 1'b0) begin
+        a_bank0_reg_l[0] <= a_bank0_dat[0];
+        a_bank0_reg_l[1] <= a_bank0_dat[1];
+        a_bank0_reg_l[2] <= a_bank0_dat[2];
+        a_bank0_reg_l[3] <= a_bank0_dat[3];
+        a_bank0_reg_l[4] <= a_bank0_dat[4];
+        a_bank0_reg_l[5] <= a_bank0_dat[5];
+        a_bank0_reg_l[6] <= a_bank0_dat[6];
+        a_bank0_reg_l[7] <= a_bank0_dat[7];
+        valid_1 <= 0;
     end
 end
 
 // ---- bank1 ---- //
 always @(posedge clk) begin
     if (sram_addr_a1[4] == 1'b1) begin
-        a_bank1_reg[0] <= a_bank1_dat[0];
-        a_bank1_reg[1] <= a_bank1_dat[1];
-        a_bank1_reg[2] <= a_bank1_dat[2];
-        a_bank1_reg[3] <= a_bank1_dat[3];
-        a_bank1_reg[4] <= a_bank1_dat[4];
-        a_bank1_reg[5] <= a_bank1_dat[5];
-        a_bank1_reg[6] <= a_bank1_dat[6];
-        a_bank1_reg[7] <= a_bank1_dat[7];
+        a_bank1_reg_h[0] <= a_bank1_dat[0];
+        a_bank1_reg_h[1] <= a_bank1_dat[1];
+        a_bank1_reg_h[2] <= a_bank1_dat[2];
+        a_bank1_reg_h[3] <= a_bank1_dat[3];
+        a_bank1_reg_h[4] <= a_bank1_dat[4];
+        a_bank1_reg_h[5] <= a_bank1_dat[5];
+        a_bank1_reg_h[6] <= a_bank1_dat[6];
+        a_bank1_reg_h[7] <= a_bank1_dat[7];
+    end
+    if (sram_addr_a1[4] == 1'b0) begin
+        a_bank1_reg_l[0] <= a_bank1_dat[0];
+        a_bank1_reg_l[1] <= a_bank1_dat[1];
+        a_bank1_reg_l[2] <= a_bank1_dat[2];
+        a_bank1_reg_l[3] <= a_bank1_dat[3];
+        a_bank1_reg_l[4] <= a_bank1_dat[4];
+        a_bank1_reg_l[5] <= a_bank1_dat[5];
+        a_bank1_reg_l[6] <= a_bank1_dat[6];
+        a_bank1_reg_l[7] <= a_bank1_dat[7];
     end
 end
 
 // ---- bank2 ---- //
 always @(posedge clk) begin
     if (sram_addr_a2[4] == 1'b1) begin
-        a_bank2_reg[0] <= a_bank2_dat[0];
-        a_bank2_reg[1] <= a_bank2_dat[1];
-        a_bank2_reg[2] <= a_bank2_dat[2];
-        a_bank2_reg[3] <= a_bank2_dat[3];
-        a_bank2_reg[4] <= a_bank2_dat[4];
-        a_bank2_reg[5] <= a_bank2_dat[5];
-        a_bank2_reg[6] <= a_bank2_dat[6];
-        a_bank2_reg[7] <= a_bank2_dat[7];
+        a_bank2_reg_h[0] <= a_bank2_dat[0];
+        a_bank2_reg_h[1] <= a_bank2_dat[1];
+        a_bank2_reg_h[2] <= a_bank2_dat[2];
+        a_bank2_reg_h[3] <= a_bank2_dat[3];
+        a_bank2_reg_h[4] <= a_bank2_dat[4];
+        a_bank2_reg_h[5] <= a_bank2_dat[5];
+        a_bank2_reg_h[6] <= a_bank2_dat[6];
+        a_bank2_reg_h[7] <= a_bank2_dat[7];
+    end
+    if (sram_addr_a2[4] == 1'b0) begin
+        a_bank2_reg_l[0] <= a_bank2_dat[0];
+        a_bank2_reg_l[1] <= a_bank2_dat[1];
+        a_bank2_reg_l[2] <= a_bank2_dat[2];
+        a_bank2_reg_l[3] <= a_bank2_dat[3];
+        a_bank2_reg_l[4] <= a_bank2_dat[4];
+        a_bank2_reg_l[5] <= a_bank2_dat[5];
+        a_bank2_reg_l[6] <= a_bank2_dat[6];
+        a_bank2_reg_l[7] <= a_bank2_dat[7];
     end
 end
 
 // ---- bank3 ---- //
 always @(posedge clk) begin
     if (sram_addr_a3[4] == 1'b1) begin
-        a_bank3_reg[0] <= a_bank3_dat[0];
-        a_bank3_reg[1] <= a_bank3_dat[1];
-        a_bank3_reg[2] <= a_bank3_dat[2];
-        a_bank3_reg[3] <= a_bank3_dat[3];
-        a_bank3_reg[4] <= a_bank3_dat[4];
-        a_bank3_reg[5] <= a_bank3_dat[5];
-        a_bank3_reg[6] <= a_bank3_dat[6];
-        a_bank3_reg[7] <= a_bank3_dat[7];
+        a_bank3_reg_h[0] <= a_bank3_dat[0];
+        a_bank3_reg_h[1] <= a_bank3_dat[1];
+        a_bank3_reg_h[2] <= a_bank3_dat[2];
+        a_bank3_reg_h[3] <= a_bank3_dat[3];
+        a_bank3_reg_h[4] <= a_bank3_dat[4];
+        a_bank3_reg_h[5] <= a_bank3_dat[5];
+        a_bank3_reg_h[6] <= a_bank3_dat[6];
+        a_bank3_reg_h[7] <= a_bank3_dat[7];
+    end
+    if (sram_addr_a3[4] == 1'b0) begin
+        a_bank3_reg_l[0] <= a_bank3_dat[0];
+        a_bank3_reg_l[1] <= a_bank3_dat[1];
+        a_bank3_reg_l[2] <= a_bank3_dat[2];
+        a_bank3_reg_l[3] <= a_bank3_dat[3];
+        a_bank3_reg_l[4] <= a_bank3_dat[4];
+        a_bank3_reg_l[5] <= a_bank3_dat[5];
+        a_bank3_reg_l[6] <= a_bank3_dat[6];
+        a_bank3_reg_l[7] <= a_bank3_dat[7];
     end
 end
 
@@ -309,54 +357,82 @@ end
 // token_sum = Σ x_i,  i = 0~15 (8 from _dat + 8 from _reg)
 // token_mean μ = token_sum / 16   // divide by 16
 
-reg [BW_PER_ACT-1:0] token1_sum;
-reg [BW_PER_ACT-1:0] token2_sum;
-reg [BW_PER_ACT-1:0] token3_sum;
-reg [BW_PER_ACT-1:0] token4_sum;
+// in sram A, data(1-bit sign + 3-bit integer + 6-bit fraction)
+// after addition 16 number, the sum is (1-bit sign + 7-bit integer + 6-bit fraction)
+// then to get mean, view the sum as (1-bit sign + 3-bit integer + 10-bit fraction)
+reg [BW_PER_ACT+3:0] token0_mean;
 reg [BW_PER_ACT+3:0] token1_mean;
 reg [BW_PER_ACT+3:0] token2_mean;
 reg [BW_PER_ACT+3:0] token3_mean;
-reg [BW_PER_ACT+3:0] token4_mean;
+reg valid_2;
 
 always @(posedge clk) begin
-    // token1 -> bank0
-    token1_sum <= a_bank0_dat[0] + a_bank0_dat[1] + a_bank0_dat[2] + a_bank0_dat[3]
+    if (valid_1) begin
+        // token0 -> bank0
+    token0_mean <= a_bank0_dat[0] + a_bank0_dat[1] + a_bank0_dat[2] + a_bank0_dat[3]
                 + a_bank0_dat[4] + a_bank0_dat[5] + a_bank0_dat[6] + a_bank0_dat[7]
-                + a_bank0_reg[0] + a_bank0_reg[1] + a_bank0_reg[2] + a_bank0_reg[3]
-                + a_bank0_reg[4] + a_bank0_reg[5] + a_bank0_reg[6] + a_bank0_reg[7];
+                + a_bank0_reg_h[0] + a_bank0_reg_h[1] + a_bank0_reg_h[2] + a_bank0_reg_h[3]
+                + a_bank0_reg_h[4] + a_bank0_reg_h[5] + a_bank0_reg_h[6] + a_bank0_reg_h[7];
 
-    // token2 -> bank1
-    token2_sum <= a_bank1_dat[0] + a_bank1_dat[1] + a_bank1_dat[2] + a_bank1_dat[3]
+    // token1 -> bank1
+    token1_mean <= a_bank1_dat[0] + a_bank1_dat[1] + a_bank1_dat[2] + a_bank1_dat[3]
                 + a_bank1_dat[4] + a_bank1_dat[5] + a_bank1_dat[6] + a_bank1_dat[7]
-                + a_bank1_reg[0] + a_bank1_reg[1] + a_bank1_reg[2] + a_bank1_reg[3]
-                + a_bank1_reg[4] + a_bank1_reg[5] + a_bank1_reg[6] + a_bank1_reg[7];
+                + a_bank1_reg_h[0] + a_bank1_reg_h[1] + a_bank1_reg_h[2] + a_bank1_reg_h[3]
+                + a_bank1_reg_h[4] + a_bank1_reg_h[5] + a_bank1_reg_h[6] + a_bank1_reg_h[7];
 
-    // token3 -> bank2
-    token3_sum <= a_bank2_dat[0] + a_bank2_dat[1] + a_bank2_dat[2] + a_bank2_dat[3]
+    // token2 -> bank2
+    token2_mean <= a_bank2_dat[0] + a_bank2_dat[1] + a_bank2_dat[2] + a_bank2_dat[3]
                 + a_bank2_dat[4] + a_bank2_dat[5] + a_bank2_dat[6] + a_bank2_dat[7]
-                + a_bank2_reg[0] + a_bank2_reg[1] + a_bank2_reg[2] + a_bank2_reg[3]
-                + a_bank2_reg[4] + a_bank2_reg[5] + a_bank2_reg[6] + a_bank2_reg[7];
+                + a_bank2_reg_h[0] + a_bank2_reg_h[1] + a_bank2_reg_h[2] + a_bank2_reg_h[3]
+                + a_bank2_reg_h[4] + a_bank2_reg_h[5] + a_bank2_reg_h[6] + a_bank2_reg_h[7];
 
-    // token4 -> bank3
-    token4_sum <= a_bank3_dat[0] + a_bank3_dat[1] + a_bank3_dat[2] + a_bank3_dat[3]
+    // token3 -> bank3
+    token3_mean <= a_bank3_dat[0] + a_bank3_dat[1] + a_bank3_dat[2] + a_bank3_dat[3]
                 + a_bank3_dat[4] + a_bank3_dat[5] + a_bank3_dat[6] + a_bank3_dat[7]
-                + a_bank3_reg[0] + a_bank3_reg[1] + a_bank3_reg[2] + a_bank3_reg[3]
-                + a_bank3_reg[4] + a_bank3_reg[5] + a_bank3_reg[6] + a_bank3_reg[7];
-end
-
-// mean has 14 bit, 10-bit number + 4-bit accuracy preserve
-always @(*) begin
-    // compute mean (division by 16)
-    token1_mean = {4'b0, token1_sum};
-    token2_mean = {4'b0, token2_sum};
-    token3_mean = {4'b0, token3_sum};
-    token4_mean = {4'b0, token4_sum};
+                + a_bank3_reg_h[0] + a_bank3_reg_h[1] + a_bank3_reg_h[2] + a_bank3_reg_h[3]
+                + a_bank3_reg_h[4] + a_bank3_reg_h[5] + a_bank3_reg_h[6] + a_bank3_reg_h[7];
+    end
+    
+    
+    valid_2 <= valid_1;
 end
 
 // ----- token MAE ----- //
 // token_mae = Σ |x_i - μ| / 16,  i = 0~15 (8 from _dat + 8 from _reg)
 
+// to get abs value, we calculate x_i(1-bit sign + 3-bit integer + 6-bit fraction)
+// minus μ (1-bit sign + 3-bit integer + 10-bit fraction)
+// the result abs data may be (1-bit sign + 3-bit integer + 10-bit fraction)
+// sum 16 as data so the result may be (1-bit sign + 7-bit integer + 10-bit fraction)
+// then devide by 16, so we view the sum as (1-bit sign + 3-bit integer + 14-bit fraction)
+// to get the MAE
+
+reg [13:0] a_bank0_minus[0:15];
+reg [13:0] a_bank1_minus[0:15];
+reg [13:0] a_bank2_minus[0:15];
+reg [13:0] a_bank3_minus[0:15];
+always @(*) begin
+    a_bank0_minus[0]  = {a_bank0_reg_h[0], 4'b0} - token0_mean; // ch0
+    a_bank0_minus[1]  = a_bank0_reg_h[1] - token0_mean; // ch1
+    a_bank0_minus[2]  = a_bank0_reg_h[2] - token0_mean; // ch2
+    a_bank0_minus[3]  = a_bank0_reg_h[3] - token0_mean; // ch3
+    a_bank0_minus[4]  = a_bank0_reg_h[4] - token0_mean; // ch4
+    a_bank0_minus[5]  = a_bank0_reg_h[5] - token0_mean; // ch5
+    a_bank0_minus[6]  = a_bank0_reg_h[6] - token0_mean; // ch6
+    a_bank0_minus[7]  = a_bank0_reg_h[7] - token0_mean; // ch7
+
+    a_bank0_minus[8]  = a_bank0_reg_l[0] - token0_mean; // ch8
+    a_bank0_minus[9]  = a_bank0_reg_l[1] - token0_mean; // ch9
+    a_bank0_minus[10] = a_bank0_reg_l[2] - token0_mean; // ch10
+    a_bank0_minus[11] = a_bank0_reg_l[3] - token0_mean; // ch11
+    a_bank0_minus[12] = a_bank0_reg_l[4] - token0_mean; // ch12
+    a_bank0_minus[13] = a_bank0_reg_l[5] - token0_mean; // ch13
+    a_bank0_minus[14] = a_bank0_reg_l[6] - token0_mean; // ch14
+    a_bank0_minus[15] = a_bank0_reg_l[7] - token0_mean; // ch15
+end
 
 // ===== Quantize the result to 10-bit and write the result to SRAM B ===== //
 
 endmodule
+
+
